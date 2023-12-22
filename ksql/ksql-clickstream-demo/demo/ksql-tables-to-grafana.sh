3adfa0024089d967e28854c2af21d96be4b42ce6
GaborDevv <147967502+GaborDevv@users.noreply.github.com>
2023-12-22 09:19:48 +0100

    Update ksql-tables-to-grafana.sh

--- ksql/ksql-clickstream-demo/demo/ksql-tables-to-grafana.sh
+++ ksql/ksql-clickstream-demo/demo/ksql-tables-to-grafana.sh
@@ -15,7 +15,7 @@ if [[ -z "${ELASTIC_HOST}" ]]; then
   ELASTIC_HOST="localhost"
 fi
 
-declare -a tables=('click_user_sessions' 'user_ip_activity' 'enriched_error_codes_count' 'errors_per_min_alert' 'errors_per_min' 'events_per_min' 'pages_per_min');
+declare -a tables=('click_user_sessions' 'user_ip_activity' 'enriched_error_codes_count' 'errors_per_min_alert' 'errors_per_min' 'events_per_min' 'pages_per_min''high_bandwidth_user_sessions');
 for i in "${tables[@]}"
 do
     table_name=$i
